27d921049137b88cbadf57adf7f5edd8e4ba6f05
thomlund <t.lundgaard@outlook.dk>
2024-11-10 20:33:33 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,42 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    mySprite.setPosition(1, 6)
     tiles.setCurrentTilemap(tilemap`level7`)
+    mySprite.setPosition(1, 6)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingPine, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level5`)
+    mySprite.setPosition(1, 6)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd4, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.shrub, function (sprite, location) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.confetti)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd0, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock0, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level20`)
     mySprite.setPosition(1, 6)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd0, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level9`)
+    mySprite.setPosition(1, 6)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingOak, function (sprite, location) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.confetti)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-    mySprite.setPosition(1, 6)
-    tiles.setCurrentTilemap(tilemap`level11`)
-})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.confetti)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd1, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock2, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level11`)
     mySprite.setPosition(1, 6)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.crowd1, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level13`)
+    mySprite.setPosition(1, 6)
 })
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level2`)
